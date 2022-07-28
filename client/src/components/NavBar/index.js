c6476136bc04cc9baae820c1414f7e1f76f04c59
jyliao369 <jyliao369@gmail.com>
2022-07-28 16:20:30 -0400

    added a register function and adjusted the style of the app

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <p>NavBar</p>
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+      <Link to="/explore">
+        <p>Explore</p>
+      </Link>
+      <Link to="/subthreads">
+        <p>Threads</p>
+      </Link>
+      <Link to="/subthreads">
+        <p>Profile</p>
+      </Link>
+      <Link to="/subthreads">
+        <p>Bookmark</p>
+      </Link>
+      <Link to="/settings">
+        <p>Settings</p>
+      </Link>
+      <Link to="/login">
+        <p>Login</p>
+      </Link>
+      <Link to="/register">
+        <p>Register</p>
+      </Link>
     </div>
   );
 };
--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,9 +1,66 @@
 import React from "react";
+import { useState } from "react";
+import Axios from "axios";
 
 const Register = () => {
+  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const registerUser = () => {
+    Axios.post("http://localhost:3001/registerUser", {
+      lastName: lastName,
+      firstName: firstName,
+      username: username,
+      email: email,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="explore">
       <p>Register</p>
+      <input
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        placeholder="Last Name"
+      />
+      <input
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="First Name"
+      />
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <input
+        value={rePassword}
+        onChange={(e) => setRePassword(e.target.value)}
+        placeholder="Re-Type Password"
+      />
+      <button
+        value={[lastName, firstName, username, email, password, rePassword]}
+        onClick={(e) => registerUser(e.target.value)}
+      >
+        Register
+      </button>
     </div>
   );
 };
