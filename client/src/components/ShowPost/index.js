1a2e81e94e94a00dae7d3d71e799eee2316b3c01
jyliao369 <jyliao369@gmail.com>
2022-07-27 23:25:16 -0400

    added more components and tested fetch

--- client/src/components/ShowPost/index.js
+++ client/src/components/ShowPost/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 const ShowPost = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://threado-server.herokuapp.com/", {}).then((response) => {
+      // console.log(response.data.reverse());
+      setAllPosts(response.data.reverse());
+    });
+  }, []);
+
   return (
     <div className="showPost">
       <p>ShowPost</p>
+      <div>
+        {allPosts.map((post) => (
+          <div key={post.postID}>
+            <p>{post.postTitle}</p>
+            <p>{post.postBody}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
